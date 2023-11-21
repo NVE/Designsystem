584569637bb81c3c948b326c48f2fbfdd06953f6
jorgenlampe <jobl@nve.no>
2023-11-21 15:29:02 +0100

    lagt til alle props fra SL

--- nve-ds/src/stories/NveButton.js
+++ nve-ds/src/stories/NveButton.js
@@ -6,11 +6,31 @@ import './button.css';
 /**
  * Primary UI component for user interaction
  */
-export const NveButton = ({ size, type, disabled, loading, label, showLabel, trailingIcon, leadingIcon }) => {
+export const NveButton = ({ size, type, disabled, loading, label, showLabel, trailingIcon, leadingIcon, 
+  variant, caret, outline, pill, circle, name, value, href, target, rel, download, form, formAction, formEnctype,
+  formMethod, formNoValidate, formTarget
 
+}) => {
 
   return html`
         <nve-button 
+    variant=${variant}
+    caret=${caret}
+    outline=${outline}
+    pill=${pill}
+    circle=${circle}
+    name=${name}
+    value=${value}
+    href=${href}
+    target=${target}
+    rel=${rel}
+    download=${download}
+    form=${form}
+    formAction=${formAction}
+    formEnctype=${formEnctype}
+    formMethod=${formMethod}
+    formNoValidate=${formNoValidate}
+    formTarget=${formTarget}
     label=${label}
     size=${size}
     type=${type}
