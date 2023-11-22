f15a5b202636d7b76226774fa4fd11b6b995cb78
marcin <marcin.janecki@bouvet.no>
2023-11-22 12:00:27 +0100

    added main.ts, material icons and added nve-icon

---
+++ lit-project/src/nve-icon.ts
@@ -0,0 +1,29 @@
+import { SlIcon } from '@shoelace-style/shoelace';
+import { customElement, property } from 'lit/decorators.js';
+import { registerIconLibrary } from '@shoelace-style/shoelace';
+import { css, html, LitElement } from 'lit';
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement('nve-icon')
+export class NveIcon extends LitElement {
+  @property() name = '';
+  static styles = css`
+    :host {
+      /* Apply Material Icons font-family to the Shadow DOM */
+      font-family: 'Material Icons';
+    }
+  `;
+  render() {
+    return html` <span class="material-icons">${this.name}</span> `;
+  }
+}
+declare global {
+  interface HTMLElementTagNameMap {
+    'nve-icon': NveIcon;
+  }
+}
