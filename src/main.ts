ffd15972b0616c3b76ea7b3a220853bb7bc6ee0c
marcin <marcin.janecki@bouvet.no>
2023-12-11 16:07:11 +0100

    added demo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 // test-applikasjon for komponentbiblioteket
 import { render } from 'lit';
 import '../build/css/varsom.css';
-import './styles/global.css';
 import './styles/testsite.css';
+import './styles/global.css';
 
 /* Importer demo-side for hver komponent du vil vise her og sett det i render-funksjonen */
 import buttonDemo from './components/nve-button/nve-button-demo';
