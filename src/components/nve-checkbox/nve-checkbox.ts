1a10c390e953534aa3fdca4dfdace73f3a520263
marcin <marcin.janecki@bouvet.no>
2023-12-15 12:16:54 +0100

    updated comment

--- src/components/nve-checkbox/nve-checkbox.ts
+++ src/components/nve-checkbox/nve-checkbox.ts
@@ -8,8 +8,7 @@ export class NveCheckbox extends SlCheckbox {
     super();
   }
   // checkbox bruker checbkox sl-icon og jeg tror vi kan la den være sånn for nå. hvis design folka sier at vi ikke burde bruke den
-  // må vi mest sannsynligvis skrive vår egen checkbox (wrapping vil ikke hjelpe fordi sl-icon er hjemt i sl-checkbox så vi må forke den for å tilpasse).
-  // foreløpig finnes ikke noe hover styling i skissene så jeg bare slår av hover styling
+  // foreløpig finnes det ikke noe hover styling i skissene så jeg bare slår av hover styling
   static styles = [SlCheckbox.styles, styles];
 }
 
