22a3b9b77b7fd0e06f18d8068586de88d4ab1108
Erik Edin <ered@nve.no>
2023-12-18 14:52:50 +0100

    progress

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './nve-button/nve-button';
 export * from './nve-dialog/nve-dialog';
+export * from './nve-icon/nve-icon';
--- src/components/nve-dialog/nve-dialog-demo.ts
+++ src/components/nve-dialog/nve-dialog-demo.ts
@@ -4,32 +4,25 @@ const table = html`
   <hr />
   <h3>nve-dialog</h3>
   <table class="demo">
-    <tbody>
-      <tr>
-        <td>Standard</td>
-        <td>
-          <nve-dialog variant="cookie" label="Vil du slette dette påbegynte skjemaet?" class="dialog-width" style="--width: 30vw;">
-            Du kan ikke angre dette etter du har slettet skjemaet. 
-            <nve-button slot="footer" variant="primary">Slett</nve-button>
-            <nve-button class="close" slot="footer" variant="neutral">Avbryt</nve-button>
-          </nve-dialog>
-        </td>
-      </tr>
-      <tr>
-        <td colspan="2">
-          <nve-button class="open-dialog" variant="neutral">Open Dialog</nve-button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-  <script>
-    const dialog = document.querySelector('.dialog-width');
-    const openButton = document.querySelector('.open-dialog');
-    const closeButton = dialog.querySelector('.close');
+  <tbody>
+    <tr>
+      <td colspan="2">
+        Warning
+        <nve-dialog variant="warning" label="Vil du slette dette påbegynte skjemaet?" class="dialog-width" style="--width: 30vw;">
+          Du kan ikke angre dette etter du har slettet skjemaet. 
+          <nve-button slot="footer" variant="primary">Slett</nve-button>
+          <nve-button class="close" slot="footer" variant="neutral">Avbryt</nve-button>
+        </nve-dialog>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2">
+        <nve-button onclick="document.querySelector('.dialog-width').setAttribute('open', 'true')" class="open-dialog" variant="neutral">Open Dialog</nve-button>
+      </td>
+    </tr>
+  </tbody>
+</table>
 
-    openButton.addEventListener('click', () => dialog.show());
-    closeButton.addEventListener('click', () => dialog.hide());
-  </script>
 `;
 
 export default table;
