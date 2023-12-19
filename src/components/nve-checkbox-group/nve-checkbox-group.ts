d590fd28da0d43dda7b75af2c4b39f9cc7a32a70
marcin <marcin.janecki@bouvet.no>
2023-12-19 14:06:41 +0100

    isvalid to boolean

--- src/components/nve-checkbox-group/nve-checkbox-group.ts
+++ src/components/nve-checkbox-group/nve-checkbox-group.ts
@@ -13,9 +13,7 @@ export class NveCheckboxGroup extends LitElement {
     super();
   }
 
-  // since html treats all existing properties on an attribute as truthy boolean type in that case would always convert to
-  // true. we want be capable of showing either true or false, thats why we have to use strings here.
-  @property() isValid: 'true' | 'false' = 'true';
+  @property({ type: Boolean }) isValid = true;
   @property() label?: string;
   @property() tooltip?: string;
   @property() orientation: 'horizontal' | 'vertical' = 'vertical';
@@ -30,7 +28,7 @@ export class NveCheckboxGroup extends LitElement {
       const assignedElements: Element[] = this.slot.assignedElements({ flatten: true });
       const nveCheckboxes: Element[] = assignedElements.filter((element) => element.localName === 'nve-checkbox');
 
-      if (this.isValid == 'false') {
+      if (this.isValid) {
         nveCheckboxes.forEach((ch) => {
           ch.setAttribute('isValid', 'false');
         });
@@ -49,9 +47,7 @@ export class NveCheckboxGroup extends LitElement {
           <nve-label value=${this.label} size="small" tooltip=${this.tooltip}></nve-label>
         </div>
         <slot class="checkbox-group__checkboxes"></slot>
-        ${this.isValid === 'false'
-          ? html`<span class="checkbox-group__error-message">${this.errorMessage || null}</span>`
-          : null}
+        ${!this.isValid ? html`<span class="checkbox-group__error-message">${this.errorMessage || null}</span>` : null}
       </div>
     `;
   }
