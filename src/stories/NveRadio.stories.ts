ffa1bfa27d55cad93bcd6af01e04c140e9e7e58e
Lars Thomas Bredland <ltb@laudi.no>
2023-12-21 11:39:37 +0100

    lagt til størrelse i storybook

--- src/stories/NveRadio.stories.ts
+++ src/stories/NveRadio.stories.ts
@@ -3,27 +3,31 @@ import { StoryObj } from '@storybook/web-components';
 import type { NveRadioGroupProps } from './NveRadio';
 import { NveRadioGroup } from './NveRadio';
 
-
 const meta = {
-    title: 'Nve/NveRadioGroup',
-    tags: ['autodocs'],
-    render: (args: NveRadioGroupProps) => NveRadioGroup(args),
-    argTypes: {
-        label: {
-            control: { type: 'text' },
-        },
-        orientation: {
-            control: { type: 'select' },
-            options: ['horizontal', 'vertical'],
-        }
+  title: 'Nve/NveRadioGroup',
+  tags: ['autodocs'],
+  render: (args: NveRadioGroupProps) => NveRadioGroup(args),
+  argTypes: {
+    label: {
+      control: { type: 'text' },
     },
-    parameters: {
-        docs: {
-            description: {
-                component: '<h2><nve-radio-group> | NveRadioGroup</h2><a href="https://github.com/NVE/Designsystem/tree/main/doc/nve-radio-group.md">API-dokumentasjon</a>',
-            }
-        }
-    }
+    orientation: {
+      control: { type: 'select' },
+      options: ['horizontal', 'vertical'],
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          '<h2><nve-radio-group> | NveRadioGroup</h2><a href="https://github.com/NVE/Designsystem/tree/main/doc/nve-radio-group.md">API-dokumentasjon</a>',
+      },
+    },
+  },
 };
 
 export default meta;
@@ -31,10 +35,11 @@ type Story = StoryObj<NveRadioGroupProps>;
 
 // eslint-disable-next-line storybook/prefer-pascal-case
 export const standard: Story = {
-    args: {
-        label: 'Radio-gruppe',
-        orientation: 'vertical',
-        disabled: false,
-        required: true
-    },
+  args: {
+    label: 'Radio-gruppe',
+    orientation: 'vertical',
+    disabled: false,
+    required: true,
+    size: 'medium',
+  },
 };
