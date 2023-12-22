db1204552a72baf5ad47c1ffe27a3ee65900492d
marcin <marcin.janecki@bouvet.no>
2023-12-22 10:34:12 +0100

    try esbuild plugin tsc

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
+import esbuildPluginTsc from 'esbuild-plugin-tsc';
 
 export default defineConfig({
   ssr: {
     external: ['lit/decorators.js'],
   },
+  plugins: [esbuildPluginTsc()],
 });
