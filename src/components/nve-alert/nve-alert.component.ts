ce5b93eebabc0cce8cadc79043f75da6cec3ec5f
Marcin Janecki <42612597+amish1188@users.noreply.github.com>
2024-01-16 09:54:37 +0100

    Update nve-alert.component.ts

--- src/components/nve-alert/nve-alert.component.ts
+++ src/components/nve-alert/nve-alert.component.ts
@@ -11,7 +11,7 @@ export default class NveAlert extends SlAlert {
   constructor() {
     super();
   }
-  /** Tykk tekst, vises helst til venstre */
+  /** Tykk tekst, vises helt til venstre */
   @property({ reflect: true }) title: string = '';
   /** Tynnere beskrivelse tekst */
   @property({ reflect: true }) text: string = '';
