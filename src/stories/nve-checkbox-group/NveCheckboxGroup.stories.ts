61112dac8e19d09e3404ba2d5065966aa89c0bc4
jorgenlampe <jobl@nve.no>
2024-01-18 13:29:01 +0100

    oppdatert SB for checkbox group

--- src/stories/nve-checkbox-group/NveCheckboxGroup.stories.ts
+++ src/stories/nve-checkbox-group/NveCheckboxGroup.stories.ts
@@ -2,6 +2,7 @@ import '../../components/nve-checkbox-group/nve-checkbox-group.component';
 import '../../components/nve-checkbox/nve-checkbox.component';
 import { StoryObj } from '@storybook/web-components';
 import { NveCheckboxGroup, NveCheckboxGroupProps } from './NveCheckboxGroup';
+import NveCheckboxGroupDoc from './NveCheckboxGroupDoc.mdx';
 
 const meta = {
   title: 'Nve/NveCheckboxGroup',
@@ -15,6 +16,7 @@ const meta = {
   },
   parameters: {
     docs: {
+      page: NveCheckboxGroupDoc,
       description: {
         component:
           '<h2><nve-checkbox-group> | NveCheckboxGroup</h2><a href="https://github.com/NVE/Designsystem/tree/main/doc/nve-checkbox.md">API-dokumentasjon</a>',
@@ -26,7 +28,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<NveCheckboxGroupProps>;
 
-export const standard: Story = {
+export const Primary: Story = {
   args: {
     disabled: false,
     invalid: false,
@@ -36,3 +38,43 @@ export const standard: Story = {
     errorMessage: 'Error message',
   },
 };
+
+export const Horizontal: Story = {
+  args: {
+    orientation: 'horizontal',
+    label: 'Horisontal',
+  },
+};
+
+export const Vertical: Story = {
+  args: {
+    orientation: 'vertical',
+    label: 'Vertikal',
+  },
+};
+
+export const Tooltip: Story = {
+  args: {
+    orientation: 'horizontal',
+    label: 'Med tooltip',
+    tooltip: "Hjelpetekst"
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    orientation: 'horizontal',
+    label: 'Disabled',
+    disabled: true
+  },
+};
+
+export const Error: Story = {
+  args: {
+    orientation: 'horizontal',
+    label: 'Error',
+    invalid: true,
+    errorMessage: "Error message"
+  },
+};
+
