00bc990a7effc8929c69df288c9012b1980713eb
jorgenlampe <jobl@nve.no>
2024-01-18 09:31:18 +0100

    ekstra stories for alert

--- src/stories/nve-alert/NveAlert.stories.ts
+++ src/stories/nve-alert/NveAlert.stories.ts
@@ -193,3 +193,66 @@ export const Notclosable = {
     open: true,
   },
 };
+
+export const Withicon = {
+  args: {
+    variant: 'success',
+    text: 'text',
+    title: 'Med ikon',
+    leftStroke: false,
+    emphasized: false,
+    closable: false,
+    showIcon: true,
+    open: true,
+  },
+};
+
+export const Withouticon = {
+  args: {
+    variant: 'success',
+    title: 'Uten ikon',
+    text: 'text',
+    leftStroke: false,
+    emphasized: false,
+    closable: false,
+    showIcon: false,
+    open: true,
+  },
+};
+
+export const Withtitle = {
+  args: {
+    variant: 'primary',
+    title: 'Kun tittel',
+    leftStroke: false,
+    emphasized: false,
+    closable: false,
+    showIcon: false,
+    open: true,
+  },
+};
+
+export const Withtext = {
+  args: {
+    variant: 'primary',
+    text: 'kun tekst',
+    leftStroke: false,
+    emphasized: false,
+    closable: false,
+    showIcon: false,
+    open: true,
+  },
+};
+
+export const Withbothtitleandtext = {
+  args: {
+    variant: 'primary',
+    title: 'Tittel',
+    text: 'og tekst',
+    leftStroke: false,
+    emphasized: false,
+    closable: false,
+    showIcon: false,
+    open: true,
+  },
+};
