b7f1374b92c324fda702e33b39bf33065c5906f7
jorgenlampe <jobl@nve.no>
2024-02-01 08:50:19 +0100

    badge SB

--- src/stories/nve-badge/NveBadge.ts
+++ src/stories/nve-badge/NveBadge.ts
@@ -4,16 +4,19 @@ export interface NveBadgeProps {
   variant: 'primary' | 'neutral' | 'warning' | 'success' | 'danger';
   size: 'medium' | 'large' | 'small';
   low: boolean;
+  name: string;
 }
 
 export const NveBadge = ({
   variant,
   size,
   low,
+  name
 }: {
   variant: string;
   size: 'medium' | 'large' | 'small';
   low: boolean;
+  name: string;
 }) => {
-  return html` <nve-badge variant=${variant} size=${size} ?low=${low}> Badge </nve-badge> `;
+  return html` <nve-badge variant=${variant} size=${size} ?low=${low}> ${name} </nve-badge> `;
 };
