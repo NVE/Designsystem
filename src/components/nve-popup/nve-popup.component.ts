d5cd906d451c75a76187573f5cdf604a2217ee68
Marcin Janecki <42612597+amish1188@users.noreply.github.com>
2024-02-05 16:37:39 +0100

    added nve-popup (#123)

---
+++ src/components/nve-popup/nve-popup.component.ts
@@ -0,0 +1,19 @@
+import { customElement } from 'lit/decorators.js';
+import { SlPopup } from '@shoelace-style/shoelace';
+
+/**
+ * En sl-popup med NVE design.
+ * Mer info: https://shoelace.style/components/popup
+ */
+@customElement('nve-popup')
+export default class NvePopup extends SlPopup {
+  constructor() {
+    super();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'nve-popup': NvePopup;
+  }
+}
