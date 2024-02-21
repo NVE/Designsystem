a0b600d9fdfda91a341214edfbb7925b1d27f0a1
Amina Brenneng <amibre@nve.no>
2024-02-21 12:52:09 +0100

    Added display for nve-menu in nve-search

--- src/components/nve-search/nve-search.demo.ts
+++ src/components/nve-search/nve-search.demo.ts
@@ -4,6 +4,49 @@
   /**
  * Demonstrasjon av nve-search
  */
+/**
+ * Metoden for å teste custom validering
+ * @param e event
+ * @returns
+ */
+
+const mockupData = [
+  "Amina",
+  "Marcin",
+  "Jørgen",
+  "Knut",
+  "Henning",
+  "Khoi"
+]
+
+document.addEventListener("DOMContentLoaded", function() {
+  const nveSearch = document.getElementById("nveSearch") as HTMLElement;
+  const nveMenu = document.querySelector("nve-menu") as HTMLElement;
+
+  nveSearch?.addEventListener("input",function(){
+    updateDropdownMenu(nveSearch, nveMenu);
+  } );
+})
+
+
+const updateDropdownMenu = (nveSearch: HTMLElement, nveMenu: HTMLElement) => {
+  nveMenu.innerHTML = '';
+  
+  const searchInputValue = nveSearch.value.toLowerCase();
+  console.log("searchInputValue", searchInputValue)
+
+  const filteredData = searchInputValue ? mockupData.filter(item => item.toLowerCase().includes(searchInputValue)) : [];
+
+  // Create and append new menu items
+  filteredData.forEach(itemText => {
+    const menuItem = document.createElement('nve-menu-item');
+    menuItem.textContent = itemText;
+    nveMenu.appendChild(menuItem);
+  });
+  //selectedSaksbehandler.value = (event.target as HTMLInputElement).value;
+};
+
+
 const table = html`
 <hr />
 <h3 id="nve-search">nve-search</h3>
@@ -30,9 +73,11 @@ const table = html`
           </nve-search>
         </td>
         <td>
-          <nve-search placeholder="Søk" variant="grey-outlined">
+          <nve-search id="nveSearch" placeholder="Søk" variant="grey-outlined">
             <nve-icon name="search" slot="prefix"></nve-icon>
           </nve-search>
+          <nve-menu>
+          </nve-menu>
         </td>
       </tr>
       <tr>
