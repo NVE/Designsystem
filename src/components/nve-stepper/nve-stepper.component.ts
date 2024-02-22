8c01b5f4449920dc1d769e47a5ea08b745ff2a56
Henning Holm <heroho@nve.no>
2024-02-22 16:02:18 +0100

    small fix

--- src/components/nve-stepper/nve-stepper.component.ts
+++ src/components/nve-stepper/nve-stepper.component.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from "lit";
+import { LitElement, html } from "lit";
 import { customElement, property, query, state } from "lit/decorators.js";
 import { StepProps, StepState } from "./nve-step/nve-step.component";
 import styles from './nve-stepper.styles';
