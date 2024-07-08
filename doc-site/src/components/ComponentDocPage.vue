2cc2835a7bff22ca838bd82fdaf65bb0bd238928
Øystein Myhre <71138449+gruble@users.noreply.github.com>
2024-07-08 12:20:46 +0200

    Installasjon av dokumentasjons-nettsted i skyen (#175)
    * Denne dekker både installasjon av produksjonsmiljø og PR-bygg-miljø.
    
    * fiks av ComponentDocPage.vue for å unngå advarsler i konsollet.
    
    * Lagt inn navigationFallback for at man skal kunne gå direkte til en under-side
    
    * Fjernet byggescript vi ikke trenger lengre

--- doc-site/src/components/ComponentDocPage.vue
+++ doc-site/src/components/ComponentDocPage.vue
@@ -1,47 +1,40 @@
-/**
- * Viser dokumentasjonssida for en komponent.
- * Inneholder 
- * - overskrift (navn på komponenten, fra JsDoc), 
- * - beskrivelse av komponenten (fra JsDoc), 
- * - hjelpetekst med kodeeksempler (fra komponentens markdown-fil),
- * - API-dokumentasjon (fra JsDoc)
- */
+/** * Viser dokumentasjonssida for en komponent. * Inneholder * - overskrift (navn på komponenten, fra JsDoc), * -
+beskrivelse av komponenten (fra JsDoc), * - hjelpetekst med kodeeksempler (fra komponentens markdown-fil), * -
+API-dokumentasjon (fra JsDoc) */
 <template>
   <div>
     <h1>{{ componentName }}</h1>
-    <ComponentDescription :component-name />
+    <ComponentDescription :component-name="componentName" />
     <WebComponent />
-    <ComponentSlots :component-name />
-    <ComponentEvents :component-name />
-    <ComponentFields :component-name />
-    <ComponentMethods :component-name />
-    <ComponentParts :component-name />
+    <ComponentSlots :component-name="componentName" />
+    <ComponentEvents :component-name="componentName" />
+    <ComponentFields :component-name="componentName" />
+    <ComponentMethods :component-name="componentName" />
+    <ComponentParts :component-name="componentName" />
   </div>
 </template>
- 
- <script setup lang="ts">
 
-    import { ref, watch } from 'vue';
-    import { useRoute } from 'vue-router';
-    import ComponentDescription from './ComponentDescription.vue';
-    import WebComponent from './WebComponent.vue';
-    import ComponentFields from './apidoc/ComponentFields.vue';
-    import ComponentMethods from './apidoc/ComponentMethods.vue';
-    import ComponentSlots from './apidoc/ComponentSlots.vue';
-    import ComponentEvents from './apidoc/ComponentEvents.vue';
-    import ComponentParts from './apidoc/ComponentParts.vue';
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import ComponentDescription from './ComponentDescription.vue';
+import WebComponent from './WebComponent.vue';
+import ComponentFields from './apidoc/ComponentFields.vue';
+import ComponentMethods from './apidoc/ComponentMethods.vue';
+import ComponentSlots from './apidoc/ComponentSlots.vue';
+import ComponentEvents from './apidoc/ComponentEvents.vue';
+import ComponentParts from './apidoc/ComponentParts.vue';
 
-    const route = useRoute();
-    const componentName = ref<string>('');
+const route = useRoute();
+const componentName = ref('');
 
-    watch(
-    () => route.params.component,
-    async (newComponent, oldComponent) => {
-        if (newComponent !== oldComponent) {
-            componentName.value = route.params.component as string;
-        }   
-    },
-    { immediate: true }
-    );
-
- </script>
+watch(
+  () => route.params.component,
+  async (newComponent, oldComponent) => {
+    if (newComponent !== oldComponent) {
+      componentName.value = route.params.component as string;
+    }
+  },
+  { immediate: true }
+);
+</script>
