e160ebd1107b7edef177801c3996d4ad2b469efd
Øystein Myhre <71138449+gruble@users.noreply.github.com>
2024-08-20 13:18:33 +0200

    Gjort bedre plass til beskrivelses-kolonna i tabellen over egenskaper

--- doc-site/.vitepress/theme/components/apidoc/ComponentFields.vue
+++ doc-site/.vitepress/theme/components/apidoc/ComponentFields.vue
@@ -10,24 +10,24 @@
   <table>
     <tr>
       <td>Navn</td>
-      <td>
-        <a href="https://lit.dev/docs/components/properties/#reflected-attributes" target="_blank">A</a>
-      </td>
       <td>Type</td>
       <td>Arvet fra</td>
       <td>Beskrivelse</td>
+      <td>
+        <a href="https://lit.dev/docs/components/properties/#reflected-attributes" target="_blank">A</a>
+      </td>
     </tr>
     <tr v-for="field in fields" :key="field.name">
       <td>{{ field.name }}</td>
-      <td>
-        <nve-icon v-if="field.reflects" name="Check" />
-      </td>
       <td>
         {{ field.type?.text }}
         <span v-if="field.default"> = {{ field.default }}</span>
       </td>
       <td>{{ field.inheritedFrom?.name }}</td>
       <td>{{ field.description }}</td>
+      <td>
+        <nve-icon v-if="field.reflects" name="Check" />
+      </td>
     </tr>
   </table>
 </template>
