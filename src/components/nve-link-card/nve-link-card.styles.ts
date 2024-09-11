35b38e90959feda1ab6efe67007982cdac3d0c7d
Joel <joel@nve.no>
2024-09-11 10:38:01 +0200

    feature(nve-link-card): Updated with small fixes

--- src/components/nve-link-card/nve-link-card.styles.ts
+++ src/components/nve-link-card/nve-link-card.styles.ts
@@ -2,7 +2,7 @@ import { css } from 'lit';
 export default css`
   .link-card {
     border-radius: var(--borderRadius-large, 6px);
-    padding: var(--spacing-medium, 16px) var(--spacing-medium, 16px) var(--spacing-medium, 16px) var(--spacing-large, 24px);
+    padding: var(--spacing-medium, 16px) var(--spacing-large, 24px);
     transition: background-color 0.3s, border-color 0.3s;
     display: flex;
     flex-direction: row;
@@ -12,41 +12,21 @@ export default css`
     cursor: pointer;
   }
 
-  .small {
+  .link-card--small {
     min-height: 40px;
     padding: var(--spacing-small, 12px) var(--spacing-medium, 16px);
   }
 
-  .medium {
+  .link-card--medium {
     min-height: 60px;
-    padding: var(--spacing-medium, 16px) var(--spacing-medium, 16px) var(--spacing-medium, 16px) var(--spacing-large, 24px);
   }
 
-  .large {
+  .link-card--large {
     min-height: 70px;
-    padding: var(--spacing-medium, 16px) var(--spacing-medium, 16px) var(--spacing-medium, 16px) var(--spacing-large, 24px);
   }
 
-  .link-card.small .title {
-    font-size: 0.875rem; 
-  }
-
-  .link-card.medium .title {
-    font-size: 1rem;  
-  }
-
-  .link-card.large .title {
-    font-size: 1.125rem; 
-  }
-
-
-  .content {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-
-  .title {
+  .link-card__title {
+    font-size: 1rem;
     color: var(--neutrals-foreground-primary, #0D0D0E);
     font-family: "Source Sans Pro";
     font-style: normal;
@@ -54,41 +34,52 @@ export default css`
     line-height: 150%;
   }
 
-  .additional-text {
+  .link-card__title--small {
+    font-size: 0.875rem;
+  }
+
+  .link-card__title--large {
+    font-size: 1.125rem;
+  }
+
+  .link-card__content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .link-card__additional-text {
     color: var(--neutrals-foreground-primary, #0D0D0E);
     font-family: "Source Sans Pro";
     font-style: normal;
     font-weight: 400;
     line-height: 120%;
     padding-top: 4px;
-    font-size: 0.875rem; 
+    font-size: 0.875rem;
   }
 
-  .primary {
+  .link-card--primary {
     background: var(--neutrals-background-primary, #FFF);
   }
 
-  .primary:hover {
+  .link-card--primary:hover {
     background: var(--neutrals-background-secondary, #E4E5E7);  
   }
 
-  .contrast {
+  .link-card--contrast {
     background: var(--neutrals-background-primary-contrast, #F7F7F8);  
   }
 
-  .contrast:hover {
+  .link-card--contrast:hover {
     background: var(--neutrals-background-secondary, #E4E5E7);  
   }
 
-  .secondary {
+  .link-card--secondary {
     background: var(--neutrals-background-secondary, #E4E5E7);  
   }
 
-  .secondary:hover {
+  .link-card--secondary:hover {
     border-radius: var(--borderRadius-large, 6px);
     border: 2px solid var(--neutrals-foreground-primary, #0D0D0E);
   }
 `;
-
-
-
