bdb1a0c00545f04d88d915beeea89c6ca3bac2bf
Joel <joel@nve.no>
2024-10-08 14:12:08 +0200

    feat: testar

--- src/components/nve-alert/nve-alert.component.ts
+++ src/components/nve-alert/nve-alert.component.ts
@@ -15,7 +15,7 @@ export default class NveAlert extends SlAlert {
     super();
   }
   
-  /** Tykk tekst, vises helt til venstre   */
+  /** Tykk tekst, vises helt til venstre     */
   @property({ reflect: true }) title: string = '';
   /** Tynnere beskrivelse tekst  */
   @property({ reflect: true }) text: string = '';
