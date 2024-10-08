47f51e03efd9ffacc8f44671f58ec08c11ce379a
Joel <joel@nve.no>
2024-10-08 12:58:25 +0200

    feat(comitizen): from comitizen

--- src/components/nve-alert/nve-alert.component.ts
+++ src/components/nve-alert/nve-alert.component.ts
@@ -14,7 +14,7 @@ export default class NveAlert extends SlAlert {
   constructor() {
     super();
   }
-  /** Tykk tekst, vises helt til venstre */
+  /** Tykk tekst, vises helt til venstre   */
   @property({ reflect: true }) title: string = '';
   /** Tynnere beskrivelse tekst */
   @property({ reflect: true }) text: string = '';
