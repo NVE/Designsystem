e6bc9014f8c5e637c2bf62630806aa7b9809b077
marcin <martin.joodd@gmail.com>
2024-12-04 12:54:01 +0100

    add token device
    fix fontSizes
    
    test

--- transformTokens/customtransforms/fontSizes.js
+++ transformTokens/customtransforms/fontSizes.js
@@ -2,11 +2,11 @@ export default {
   name: 'fontsizes/px',
   type: 'value',
   transitive: true,
-  matcher: function (token) {
+  filter: function (token) {
     const match = token.type === 'fontSizes';
     return match;
   },
-  transformer: function (token) {
-    return !isNaN(token.value) ? `${token.value}px` : token.value;
+  transform: function (token) {
+    return token.value;
   },
 };
