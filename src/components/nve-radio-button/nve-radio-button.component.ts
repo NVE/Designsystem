63c667128b5462559549004d0c5e03af7d1995d0
Tom Are Pedersen <tomped@nve.no>
2025-01-23 07:37:46 +0100

    Endret litt på rekkefølge i nve-radio-button slik at jsdoc kom korrekt på DS-websiden

--- src/components/nve-radio-button/nve-radio-button.component.ts
+++ src/components/nve-radio-button/nve-radio-button.component.ts
@@ -1,17 +1,20 @@
 import { customElement, property } from 'lit/decorators.js';
 import SlRadioButton from '@shoelace-style/shoelace/dist/components/radio-button/radio-button.js';
+import styles from './nve-radio-button.styles';
 
 /**
  * En sl-radio-button med NVE design.
  * Se https://shoelace.style/components/radio-button
+ *
  *
  * Merk at denne har ekstra hjørne-styling på første og siste knapp i gruppen.
  * Man må derfor unngå å ha andre elementer blant nve-radio-button
  *
  * I motsetning til vanlig button så har denne støtte for "pill", men gjelder også da kun første og siste knapp
  *
+ *  @extends SlRadioGroup
  */
-import styles from './nve-radio-button.styles';
+
 @customElement('nve-radio-button')
 export default class NveRadioButton extends SlRadioButton {
   constructor() {
