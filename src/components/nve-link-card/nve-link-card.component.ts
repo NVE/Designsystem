2de921718703a8a331a9cc38149d3b4a0b5653f6
NVEJoel <joel@nve.no>
2025-01-29 11:37:17 +0100

    feat: adding step feature (#378)

--- src/components/nve-link-card/nve-link-card.component.ts
+++ src/components/nve-link-card/nve-link-card.component.ts
@@ -106,7 +106,6 @@ export default class NveLinkCard extends LitElement implements INveComponent {
   }
   /**
    * Returnerer ikonnavnet som vises på kortet basert på clickAction.
-   * 
    */
   private getIconName() {
     switch (this.clickAction) {
