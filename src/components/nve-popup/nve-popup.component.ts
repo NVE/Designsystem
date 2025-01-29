30def89181ca29e5b190be9f48e68229a2e661b3
Joel <joel@nve.no>
2025-01-29 15:39:43 +0100

    fixed docs

--- src/components/nve-popup/nve-popup.component.ts
+++ src/components/nve-popup/nve-popup.component.ts
@@ -2,7 +2,9 @@ import { customElement } from 'lit/decorators.js';
 import SlPopup from '@shoelace-style/shoelace/dist/components/popup/popup.js';
 
 /**
- * TODO: Mangler beskrivelse
+ * Popup er et verktøy som lar deg deklarativt forankre "popup"-beholdere til et annet element.
+ * Popup gir ingen stiler - bare posisjonering! 
+ * Popup-vinduets foretrukne plassering, avstand og skrens (offset) kan konfigureres ved hjelp av attributter. 
  */
 @customElement('nve-popup')
 export default class NvePopup extends SlPopup {
