35da7d527993d2b59e57fbeabf9fefe7b5ee644d
NVEJoel <joel@nve.no>
2025-02-06 14:32:27 +0100

    feat(docs): titel håper jeg (#396)
    nej skal ikke med
    
    BREAKING CHANGE: Breaking bad

--- src/components/nve-popup/nve-popup.component.ts
+++ src/components/nve-popup/nve-popup.component.ts
@@ -3,6 +3,9 @@ import SlPopup from '@shoelace-style/shoelace/dist/components/popup/popup.js';
 
 /**
  * TODO: Mangler beskrivelse
+ * 
+ * 
+ * 
  */
 @customElement('nve-popup')
 export default class NvePopup extends SlPopup {
