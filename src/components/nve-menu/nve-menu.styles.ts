2f1b4db2dcc30ab881ad3b688b83773a92a111fe
Tom Pedersen <1415522+tomapedersen@users.noreply.github.com>
2025-02-06 10:24:31 +0100

    Fix 391 . Feil farge på nve-divider i nve-menu (#392)

--- src/components/nve-menu/nve-menu.styles.ts
+++ src/components/nve-menu/nve-menu.styles.ts
@@ -7,13 +7,13 @@ export default css`
     box-shadow: var(--soft);
   }
   ::slotted(nve-label) {
-    padding: var(--spacing-small, 0.75rem);
-    color: var(--neutrals-foreground-subtle, #006b99);
+    padding: var(--spacing-small);
+    color: var(--neutrals-foreground-subtle);
     font: var(--label-x-small);
   }
   ::slotted(nve-divider) {
-    border-top: var(--borderWidth-strong, 2px) solid var(--Neutrals-Border-Subtle, #c8eaf9);
-    opacity: var(--borderWidth-default, 1);
+    border-top: var(--border-width-strong) solid var(--neutrals-border-subtle);
+    opacity: 1;
     margin-top: 1px;
     margin-bottom: 1px;
   }
