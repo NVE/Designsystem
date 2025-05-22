c5de544cc785a655978876ca0260fd669853ef51
Joel <joel@nve.no>
2025-05-22 14:31:57 +0200

    Testing new builder

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import { extname, relative, resolve } from 'path';
 import { fileURLToPath } from 'node:url';
@@ -9,9 +8,7 @@ import type { Plugin } from 'vite';
 import fs from 'fs';
 import path from 'path';
 
-/** Inneholder alle stiene til filene som skal bundles, og skal ha types */
 const includedPaths = ['src/**/*.ts'];
-/** Inneholder stiene til filene som ikke skal bundles, og som ikke skal ha types */
 const excludedPaths = [
   'src/components/**/*.demo.ts',
   'src/stories/**',
@@ -21,7 +18,6 @@ const excludedPaths = [
   'src/main.ts',
 ];
 
-// Sikre at vi henter riktig css fil i global.css etter build
 function replaceImportAfterBuild() {
   return {
     name: 'replace-import-path',
@@ -31,47 +27,46 @@ function replaceImportAfterBuild() {
         let content = fs.readFileSync(globalCssPath, 'utf8');
         content = content.replace('../../dist/nve-designsystem.css', '../nve-designsystem.css');
         fs.writeFileSync(globalCssPath, content, 'utf8');
-        console.log('✅ Fixed import path in global.css');
       }
     },
   };
 }
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      dts({
-        include: includedPaths,
-        exclude: excludedPaths,
-      }),
-      alias({
-        entries: [{ find: '@interfaces', replacement: resolve(__dirname, 'src/interfaces') }],
-      }) as Plugin,
-      replaceImportAfterBuild(),
-    ],
-    build: {
-      sourcemap: mode === 'development' ? true : false,
-      lib: {
-        entry: resolve(__dirname, 'src/nve-designsystem.ts'),
-        formats: ['es'],
-      },
-      rollupOptions: {
-        input: Object.fromEntries(
-          glob
-            .sync(includedPaths, {
-              ignore: excludedPaths,
-            })
-            .map((file) => [
-              // from src/components/button/button.tsx => components/button/button
-              relative('src', file.slice(0, file.length - extname(file).length)),
-              fileURLToPath(new URL(file, import.meta.url)),
-            ])
-        ),
-        output: {
-          entryFileNames: '[name].js',
-          chunkFileNames: 'chunks/[name].js',
-        },
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    dts({
+      entryRoot: 'src/components',
+      include: ['src/components'],
+      outDir: 'dist/components',
+      insertTypesEntry: false,
+      cleanVueFileName: true,
+      rollupTypes: false,
+    }),
+    alias({
+      entries: [{ find: '@interfaces', replacement: resolve(__dirname, 'src/interfaces') }],
+    }) as Plugin,
+    replaceImportAfterBuild(),
+  ],
+  build: {
+    sourcemap: mode === 'development',
+    minify: false, 
+    lib: {
+      entry: resolve(__dirname, 'src/nve-designsystem.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      input: Object.fromEntries(
+        glob
+          .sync(includedPaths, { ignore: excludedPaths })
+          .map((file) => [
+            relative('src', file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: 'chunks/[name].js',
       },
     },
-  };
-});
+  },
+}));
