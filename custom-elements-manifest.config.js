c3ccac2c10900699ace850e89bf7d7199078f3a7
gruble <omy@nve.no>
2025-10-09 13:00:39 +0200

    Ikke ta med private felter og metoder i manifestet

--- custom-elements-manifest.config.js
+++ custom-elements-manifest.config.js
@@ -40,6 +40,24 @@ export default {
   dependencies: true,
   plugins: [
     cemInheritancePlugin(cemInheritancePluginOptions),
+    // Ekskluder interne metoder og properties fra manifestet (også arvet fra eksterne manifester)
+    {
+      name: 'exclude-internal-members',
+      packageLinkPhase({ customElementsManifest }) {
+        customElementsManifest.modules?.forEach((mod) => {
+          mod.declarations?.forEach((declaration) => {
+            if (declaration.members) {
+              declaration.members = declaration.members.filter(
+                (member) =>
+                  member.privacy !== 'private' && // TS private
+                  member.privacy !== 'protected' &&
+                  !member.name?.startsWith('#') // JS private
+              );
+            }
+          });
+        });
+      },
+    },
     {
       // Legg properties som er dekorert med @property til attributes-delen i manifest-fila
       name: 'add-attributes-with-jsdoc',
