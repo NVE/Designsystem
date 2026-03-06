1a99c067314232a730f53ced342d7a2f7707c267
Ole Martin Borge <olmabo@nve.no>
2026-03-06 13:37:46 +0100

    feat(typography): Initielt oppsett av nve-heading og nve-paragraph

---
+++ src/components/nve-heading/nve-heading.test.ts
@@ -0,0 +1,19 @@
+import { afterAll, describe, expect, it } from 'vitest';
+import { fixture, fixtureCleanup } from '@open-wc/testing';
+import { html } from 'lit';
+import NveHeading from './nve-heading.component';
+
+if (!customElements.get('nve-heading')) {
+  customElements.define('nve-heading', NveHeading);
+}
+
+describe('nve-heading', () => {
+  afterAll(() => {
+    fixtureCleanup();
+  });
+
+  it('is attached to the DOM', async () => {
+    const el = await fixture<NveHeading>(html`<nve-heading></nve-heading>`);
+    expect(document.body.contains(el)).toBe(true);
+  });
+});
