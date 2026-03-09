c969c33f54525f37ee4d4900383d29a52c40409a
Ole Martin Borge <olmabo@nve.no>
2026-03-09 14:53:31 +0100

    feat(nve-link-card): lagt til icon-slot og oppdatert styling og dokumentasjon

--- src/components/nve-link-card/nve-link-card.styles.ts
+++ src/components/nve-link-card/nve-link-card.styles.ts
@@ -1,124 +1,131 @@
 import { css } from 'lit';
 export default css`
   .link-card {
-    border-radius: var(--border-radius-small);
-    padding: var(--spacing-medium, 16px) var(--spacing-large, 24px);
-    transition: background 0.3s ease;
-    border: 2px solid transparent;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    box-sizing: border-box;
+    width: 100%;
+    padding: var(--spacing-medium, 16px) var(--spacing-large, 24px);
+    gap: var(--spacing-medium, 16px);
+    border-radius: var(--border-radius-small);
+    border: var(--border-width-stronger) solid transparent;
+    background: var(--color-neutrals-background-primary);
     cursor: pointer;
     text-decoration: none;
+    transition:
+      background 0.3s ease,
+      border-color 0.3s ease;
   }
 
-  /*bruker color på a, ikke på link-card fordi link-card overskriver standard lenke farge i remmeverker som next js som har sin egen
-  wrappar rundt lenke og som støtter :visited*/
+  /* Setter farge på <a> for å sikre riktig lenkefarge uansett rammeverk eller browser */
   a {
     color: var(--color-neutrals-foreground-primary, #0d0d0e);
   }
 
-  .link-card--visited,
-  a:visited {
-    color: var(--color-interactive-links-visited);
-  }
-
-  .link-card:focus {
-    outline: var(--color-interactive-primary-foreground-border-focus, #008ffb) solid 2px;
-  }
-
-  .link-card:hover .link-card__label {
-    text-decoration: underline;
+  .link-card:hover {
+    border-color: var(--color-neutrals-border-subtle);
+    background: var(--color-neutrals-background-primary, #fff);
   }
 
   .link-card:active {
-    outline: var(--color-interactive-primary-foreground-border-focus, #008ffb) solid 2px;
+    border-color: var(--color-neutrals-border-mute);
+  }
+
+  .link-card:focus-visible {
+    outline: var(--color-interactive-primary-border-focus) solid 2px;
   }
 
   .link-card--small {
-    padding: var(--spacing-small) var(--spacing-medium) var(--spacing-small) var(--spacing-medium);
+    padding: var(--spacing-small) var(--spacing-medium);
+    gap: var(--spacing-x-small, 8px);
     min-height: var(--sizing-2x-small);
   }
 
   .link-card--medium {
-    padding: var(--spacing-medium) var(--spacing-medium) var(--spacing-medium) var(--spacing-large);
+    padding: var(--spacing-medium) var(--spacing-large);
     min-height: var(--sizing-2x-small);
   }
 
   .link-card--large {
-    padding: var(--spacing-medium) var(--spacing-medium) var(--spacing-medium) var(--spacing-large);
+    padding: var(--spacing-large);
     min-height: var(--sizing-x-small);
-  }
-
-  .link-card__label {
-    font-size: 1rem;
-    font-family: 'Source Sans 3';
-    font-style: normal;
-    font-weight: 400;
-    line-height: 150%;
-  }
-
-  .link-card--visited .link-card__label,
-  .link-card:visited .link-card__label {
-    color: var(--color-interactive-links-visited);
-  }
-
-  .link-card__label--small {
-    font-size: 0.875rem;
-  }
-
-  .link-card__label--large {
-    font-size: 1.125rem;
-  }
-
-  .link-card__content {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-
-  .link-card__additional-text {
-    color: inherit;
-    font-family: 'Source Sans 3';
-    font-style: normal;
-    font-weight: 400;
-    line-height: 120%;
-    padding-top: 4px;
-    font-size: 0.875rem;
-  }
-
-  .link-card--visited .link-card__additional-text,
-  a:visited .link-card__additional-text {
-    color: var(--color-interactive-links-visited);
   }
 
   .link-card--primary {
     background: var(--color-neutrals-background-primary, #fff);
   }
 
-  .link-card--primary:hover {
-    background: var(--color-neutrals-background-secondary, #e4e5e7);
-  }
-
   .link-card--contrast {
     background: var(--color-neutrals-background-primary-contrast, #f7f7f8);
-  }
-
-  .link-card--contrast:hover {
-    background: var(--color-neutrals-background-secondary, #e4e5e7);
   }
 
   .link-card--secondary {
     background: var(--color-neutrals-background-secondary, #e4e5e7);
   }
 
-  .link-card--secondary:hover {
-    border: 2px solid var(--color-neutrals-foreground-primary, #0d0d0e);
+  .link-card__content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1;
+  }
+
+  .link-card__label {
+    font: var(--typography-heading-x-small);
+    color: var(--color-neutrals-foreground-primary, #141414);
+    transition:
+      color 0.3s ease,
+      text-decoration-thickness 0.3s,
+      text-underline-offset 0.3s;
+  }
+
+  .link-card:hover .link-card__label {
+    color: var(--color-brand-foreground-primary);
+    text-decoration: underline;
+    text-decoration-style: solid;
+    text-decoration-thickness: 5%;
+    text-underline-offset: 16%;
+  }
+
+  .link-card:active .link-card__label {
+    text-decoration-thickness: 10%;
+    text-underline-offset: 16%;
+  }
+
+  .link-card--small .link-card__label {
+    font: var(--typography-label-small);
+  }
+
+  .link-card--large .link-card__label {
+    font: var(--typography-heading-x-small);
+  }
+
+  .link-card__additional-text {
+    color: var(--color-neutrals-foreground-subtle, #60656c);
+    font: var(--typography-body-compact-x-small-compact);
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    text-overflow: ellipsis;
+  }
+
+  .link-card--small .link-card__additional-text {
+    font: var(--typography-body-compact-x-small-compact);
+  }
+
+  .link-card--large .link-card__additional-text {
+    font: var(--typography-body-compact-small-compact);
+  }
+
+  .link-card ::slotted([slot='icon']) {
+    --icon-size: 24px;
   }
 
   nve-icon {
     --icon-size: 24px;
-    color: var(--color-neutrals-foreground-primary);
+    color: var(--color-brand-foreground-primary);
   }
 `;
