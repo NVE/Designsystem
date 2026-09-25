522acb823bd2e86de00cc33ea8d7d95a09dd92d3
Tom Are Pedersen <tomped@nve.no>
2026-09-25 09:33:28 +0200

    disabled-styling også for '+n'-indikatoren. Takk, copilot

--- src/components/nve-combobox/nve-combobox.styles.ts
+++ src/components/nve-combobox/nve-combobox.styles.ts
@@ -41,7 +41,8 @@ export default css`
     input {
       color: var(--color-interactive-foreground-tertiary-disabled);
     }
-    .combobox__value__tag {
+    .combobox__value__tag,
+    .combobox__value__indicator {
       border-color: var(--color-interactive-border-tertiary-disabled);
       background: var(--color-interactive-background-tertiary-disabled);
       > nve-icon {
